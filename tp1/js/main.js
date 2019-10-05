3624906c772fde4cedc1a30d16e08e1c12822995
ObaniGemini <robin@pi-et-ro.net>
2019-10-05 16:58:31 +0200

    Add working stuff

--- tp1/js/main.js
+++ tp1/js/main.js
@@ -1,7 +1,12 @@
-var imgSrc = ["images/image1.jpg", "images/image2.jpg"];
+const imgSrc = ["images/image1.jpg", "images/image2.jpg"];
+const lastF = 60, lastF2 = 120; //lastFrames
 var pics = [];
 var current = 0;
 var animating = false;
+
+
+var canvas;
+var context;
 
 var i = 0;
 var W, H;
@@ -19,21 +24,22 @@ window.addEventListener("load", event => {
 window.addEventListener("click", clickEvent );
 
 
-window.addEventListener("resize", updateImg );
+window.addEventListener("resize", event => { updateImg(); } );
 
 
 
 const main = event => {
 	imgSrc.forEach( (element) => {
-		let image = document.createElement("img");
-		image.setAttribute("src", element);
-		pics.push(image);
+		let img = document.createElement("img");
+		img.setAttribute("src", element);
+		pics.push(img);
 	})
 
 
-	let square = document.createElement("canvas");
-	square.setAttribute("id", "canvas");
-	document.body.appendChild(square);
+	canvas = document.createElement("canvas");
+	canvas.setAttribute("id", "canvas");
+	context = canvas.getContext("2d");
+	document.body.appendChild(canvas);
 
 	updateImg();
 }
@@ -62,27 +68,19 @@ function clickEvent( event ) {
 
 const draw = () => {
 	console.log(pics[current].getAttribute("src")); //test
-	if( i++ < 60 ) {
-		let canvas = document.getElementById("canvas");
-		let context = canvas.getContext("2d");
-
-		let imW = pics[current].naturalWidth;
-		let imH = pics[current].naturalHeight;
+	if( i++ <= lastF ) {
+		let imW = pics[current].naturalWidth/cWn;
+		let imH = pics[current].naturalHeight/cHn;
 
 		for( let j = 1; j <= cWn; j++ ) {
 			for( let k = 1; k <= cHn; k++ ) {
-				context.drawImgage(pics[current], 
-					j*(imW/cWn) - (imW/cWn)*i/120, k*(imH/cHn) - (imW/cHn)*i/120,
-					i*(imW/cWn), i*(imH/cHn),
-					j*cW - cW*i/120, k*cH - cH*i/120,
-					i*cW/60, i*cH/60);
+				context.drawImage(pics[current], 
+					j*imW - imW*i/lastF2 - imW/2, k*imH - imH*i/lastF2 - imH/2,
+					i*imW/lastF, i*imH/lastF,
+					j*cW - cW*i/lastF2 - cW/2, k*cH - cH*i/lastF2 - cH/2,
+					i*cW/lastF, i*cH/lastF);
 			};
 		};
-
-		/*context.drawImage(pics[current], imW/2 - i*imW/120, imH/2 - i*imH/120,
-										 i*imW/60, i*imH/60,
-										 W/2 - i*W/120, H/2 - i*H/120,
-										 i*W/60, i*H/60);*/
 
 		window.requestAnimationFrame(draw);
 	} else {
@@ -115,9 +113,7 @@ const updateImg = event => {
 	cW = W/cWn;
 	cH = H/cHn;
 
-	let canvas = document.getElementById("canvas");
 	canvas.setAttribute("width", W);
 	canvas.setAttribute("height", H);
-	let context = canvas.getContext("2d");
 	context.drawImage(pics[current], 0, 0, W, H);
 }
